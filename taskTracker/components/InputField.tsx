b6bfb1ab696caff617ea9693148371da659e86a6
Chigo <72313512+coolchigi@users.noreply.github.com>
2024-03-05 07:11:41 +0000

    Add new component and app styling

---
+++ taskTracker/components/InputField.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const InputField = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Add Task"/>
+            <button>Add</button>
+        </div>
+    )
+}
+
+
+export default InputField
--- taskTracker/src/App.tsx
+++ taskTracker/src/App.tsx
@@ -1,44 +1,15 @@
+import React from 'react'
+import InputField from '../components/InputField'
 import './App.css'
 
-let name: string;
-let age: number;
-let isStud: boolean;
-let hobbies: string[];
 
-type Person={
-  name: string;
-  age: number | string;
-}
-
-let person: Person ={
-  name: "Chigo",
-  age: 21
-};
-
-let person2: Person ={
-  name: "Chigo",
-  age: "Twenty-two"
-};
-
-let lotsOfPeople: Person[];
-
-function printName(name: string){
-  console.log(name)
-}
-
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-       
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+    <div className="App">
+      <span className="card-header">Task Tracker</span>
+      <InputField/> 
+    </div>
+  );
 }
 
 export default App
